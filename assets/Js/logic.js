9ca6612099eef65959fa6769ebbfadf160797275
bash <bjibril123@live.com>
2023-09-01 22:06:04 +0100

    i have added the variables, html element, the audio sound and the function into the logic.js file

--- assets/Js/logic.js
+++ assets/Js/logic.js
@@ -7,3 +7,46 @@ let timerID;
 let questionsElement = document.getElementById("questions");
 let timerElement = document.getElementById("time");
 let choiceElement = document.getElementById("choices");
+let submitButton = document.getElementById("submit");
+let startButton = document.getElementById("start");
+let initialElement = document.getElementById("initials");
+let feedBackElement = document.getElementById("feedback");
+
+// the audio sound for  correct and incorrect
+let sfxRight = new Audio("assets/sfx/correct.wav");
+let sfxWrong = new Audio("assets/sfx/incorrect.wav");
+
+
+// add the functions 
+
+function getQuestion() {
+
+}
+
+function questionClick() {
+
+}
+
+function startQuiz() {
+    alert("hi")
+}
+
+function quizEnd() {
+
+}
+
+function clockTick() {
+
+}
+
+function saveHighScore() {
+
+}
+
+function checkForEnter(Event) {
+
+}
+
+startButton.addEventListener("click", startQuiz);
+submitButton.addEventListener("click", saveHighScore);
+initialElement.addEventListener("keyup", checkForEnter);
